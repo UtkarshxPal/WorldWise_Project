1fdcb079c1fb1bc08e4f0db162f8274e5acf510f
Utkarsh Pal <Utkarshpal8800@gmail.com>
2025-01-25 02:08:03 +0530

    updated base url

--- Frontend/src/pages/AppLayout.jsx
+++ Frontend/src/pages/AppLayout.jsx
@@ -1,17 +1,17 @@
-import Map2 from "../Components/Map2";
-import PageNav from "../Components/PageNav";
-import Sidebar from "../Components/Sidebar";
-import User from "../Components/User";
-import styles from "./appLayout.module.css";
+// import Map2 from "../Components/Map2";
+// import PageNav from "../Components/PageNav";
+// import Sidebar from "../Components/Sidebar";
+// import User from "../Components/User";
+// import styles from "./appLayout.module.css";
 
-function AppLayout() {
-  return (
-    <div className={styles.app}>
-      <Sidebar></Sidebar>
-      <Map2></Map2>
-      <User></User>
-    </div>
-  );
-}
+// function AppLayout() {
+//   return (
+//     <div className={styles.app}>
+//       <Sidebar></Sidebar>
+//       <Map2></Map2>
+//       <User></User>
+//     </div>
+//   );
+// }
 
-export default AppLayout;
+// export default AppLayout;
--- Frontend/src/pages/LayoutApp.jsx
+++ Frontend/src/pages/LayoutApp.jsx
@@ -2,7 +2,8 @@ import Map2 from "../Components/Map2";
 import PageNav from "../Components/PageNav";
 import Sidebar from "../Components/Sidebar";
 import User from "../Components/User";
-import styles from "./appLayout.module.css";
+// import styles from "./appLayout.module.css";
+import styles from "./LayoutApp.module.css";
 
 function LayoutApp() {
   return (
