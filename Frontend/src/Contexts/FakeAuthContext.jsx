4d198e729202667d8dc477ef0a75dcffe8dd59da
Utkarsh Pal <Utkarshpal8800@gmail.com>
2025-02-01 04:19:52 +0530

    First commit

--- Frontend/src/Contexts/FakeAuthContext.jsx
+++ Frontend/src/Contexts/FakeAuthContext.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 import { createContext, useContext, useEffect, useReducer } from "react";
 import { useNavigate } from "react-router-dom";
 
-// const BASE_URL = "https://travel-management-worldwise-backend.onrender.com";
-const BASE_URL = " http://localhost:3000";
+const BASE_URL = " https://worldwise-qg89.onrender.com";
+// const BASE_URL = " http://localhost:3000";
 
 const AuthContext = createContext();
 
